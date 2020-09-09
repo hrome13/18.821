b9e0e4d01ec1423f44e194ed4266f513701c68f4
Hayden Rome <hrome@mit.edu>
2020-09-09 10:33:03 -0400

    Added length four grid search.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def run_game(sequence, limit=100):
+def run_game(sequence, limit=100, verbose=True):
     def run_step(seq):
         new = []
         for n in range(len(seq) - 1):
@@ -17,19 +17,46 @@ def run_game(sequence, limit=100):
             first_indices[tuple(current)] = step
         else:
             res = monster[first_indices[tuple(current)]:]
-            print("\nNever ending")
-            print(len(res)-1, "steps in the cycle")
-            for n in res:
-                print(n)
-            return step, res
+            if verbose:
+                print("\nNever ending.", step-len(res), "until reaching cycle.")
+                print(len(res)-1, "steps in the cycle.")
+                for n in res:
+                    print(n)
+            return step, res, False
         step += 1
-    print("Ends")
-    return step, [current]
+    if verbose:
+        print("Ends in", step-1, "steps")
+        print(monster)
+    return step-1, [current], True
+
+def length_four_grid_search(upper_int_limit, step_limit=100000):
+    never_ending_sum = 0
+    ending_sum = 0
+    used = set()
+    for a in range(upper_int_limit):
+        if a%10 == 0:
+            print("Current first term:", a)
+        for b in range(upper_int_limit):
+            for c in range(upper_int_limit):
+                for d in range(upper_int_limit):
+                    sequence = [a, b, c, d]
+                    if tuple(sequence) not in used:
+                        for n in range(4):
+                            perm = sequence[n:]
+                            perm.extend(sequence[:n])
+                            used.add(tuple(perm))
+                        if not run_game(sequence, step_limit, verbose=False)[2]:
+                            never_ending_sum += 1
+                        else:
+                            ending_sum += 1
+    print(never_ending_sum, "sequences found that never end.")
+    print(ending_sum, "sequences found that end (not counting their permutations).")
+    return never_ending_sum, ending_sum
 
 
 if __name__ == '__main__':
     # Change this sequence to whatever you want
-    sequence = [1,1,1,1,1,1,0,0,0,0,0,0]
-    step_limit = 1000
+    sequence = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    step_limit = 100000
     res = run_game(sequence, step_limit)
-    print(res[0], "steps until this repeat")
+    # length_four_grid_search(90)
